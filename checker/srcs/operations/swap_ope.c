0ff07478afa81d1bbe0fbe922864256633a4896d
mlarboul <mlarboul@student.42.fr>
2021-05-12 10:46:09 +0200

    sa, sb and ss OK

---
+++ checker/srcs/operations/swap_ope.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_ope.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/12 09:00:39 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/05/12 09:39:18 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/checker.h"
+
+int	single_swap(t_stack *stack)
+{
+	int	tmp_value;
+
+	if (stack == NULL || stack->tab == NULL)
+		return (-1);
+	if (stack->cur_size == 0 || stack->cur_size == 1)
+		return (0);
+	tmp_value = stack->tab[0];
+	stack->tab[0] = stack->tab[1];
+	stack->tab[1] = tmp_value;
+	return (0);
+}
+
+int	double_swap(t_stack *stack_a, t_stack *stack_b)
+{
+	if (single_swap(stack_a) < 0)
+		return (-1);
+	if (single_swap(stack_b) < 0)
+		return (-1);
+	return (0);
+}
