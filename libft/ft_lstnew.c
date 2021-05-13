89801f89e1661132763862675bef6c8041fa7c7d
mlarboul <mlarboul@student.42.fr>
2021-05-13 11:00:14 +0200

    Changing Makefile

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/10 18:35:01 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/11/10 18:35:02 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*elem;
+
+	if (!(elem = malloc(sizeof(t_list))))
+		return (0);
+	elem->content = content;
+	elem->next = 0;
+	return (elem);
+}
