c3fa33daa868da76c3c8b7d9c098c5cebc8424ae
mlarboul <mlarboul@student.42.fr>
2021-05-15 14:56:01 +0200

    I made the difference between completed and ordered list

--- push_swap_prog/srcs/utils/check_list_order.c
+++ push_swap_prog/srcs/utils/check_list_order.c
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 07:21:42 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/05/15 09:08:53 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/05/15 14:47:34 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,18 @@ t_bool	list_is_sorted(t_stack stack_a)
 	int	i;
 
 	i = 0;
-	if (stack_a.cur_size != stack_a.max_size)
-		return (FALSE);
 	while (i < stack_a.cur_size - 1)
 	{
 		if (stack_a.tab[i] >= stack_a.tab[i + 1])
 			return (FALSE);
 		i++;
 	}
+	return (TRUE);
+}
+
+t_bool	list_is_complete(t_stack stack_a)
+{
+	if (stack_a.cur_size != stack_a.max_size)
+		return (FALSE);
 	return (TRUE);
 }
