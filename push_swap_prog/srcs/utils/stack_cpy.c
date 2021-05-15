46d27999119a97c2eb422ddc3cf2b488a7c331a4
mlarboul <mlarboul@student.42.fr>
2021-05-15 14:55:28 +0200

    Useful for the next part of the algo

---
+++ push_swap_prog/srcs/utils/stack_cpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_cpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/15 10:34:13 by mlarboul          #+#    #+#             */
+/*   Updated: 2021/05/15 11:58:12 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+int	stack_cpy(t_stack *src, t_stack *dst)
+{
+	int i;
+
+	i = 0;
+	if (dst == NULL || src->tab == NULL || src->cur_size == 0)
+		return (-1);
+	dst->tab = malloc(sizeof(int) * src->cur_size);
+	if (dst->tab == NULL)
+		return (-1);
+	while (i < src->cur_size)
+	{
+		dst->tab[i] = src->tab[i];
+		i++;
+	}
+	dst->cur_size = src->cur_size;
+	dst->max_size = src->cur_size;
+	return (0);
+}
