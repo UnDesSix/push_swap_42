fac1b918a189ef75fd19254cc3e4d0d28a8bd3ef
mlarboul <mlarboul@student.42.fr>
2021-05-20 08:54:49 +0200

    Normed v3

--- checker_prog/includes/checker.h
+++ checker_prog/includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 06:58:32 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/05/16 10:17:28 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/05/20 08:54:26 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,32 @@
 # define TRUE 1
 # define FALSE 0
 
-typedef int		t_bool;
+typedef int	t_bool;
 
-typedef struct	s_stack
+typedef struct s_stack
 {
 	int		*tab;
 	int		cur_size;
 	int		max_size;
 }				t_stack;
 
-int				ft_atoi_spe(const char *str, int *result);
-int				*parse_arg(int list_size, char **argv);
-t_bool			unique_values(int *list, int list_size);
-t_bool			operation_is_valid(char *ope);
-t_bool			list_is_sorted(t_stack stack_a);
+int		ft_atoi_spe(const char *str, int *result);
+int		*parse_arg(int list_size, char **argv);
+t_bool	unique_values(int *list, int list_size);
+t_bool	operation_is_valid(char *ope);
+t_bool	list_is_sorted(t_stack stack_a);
 
-int				create_stack_a(t_stack *stack_a, int *list, int list_size);
-int				create_stack_b(t_stack *stack_b, int list_size);
+int		create_stack_a(t_stack *stack_a, int *list, int list_size);
+int		create_stack_b(t_stack *stack_b, int list_size);
 
-int				single_swap(t_stack *stack);
-int				double_swap(t_stack *stack_a, t_stack *stack_b);
-int				push_ope(t_stack *src, t_stack *dst);
-int				single_rotate(t_stack *stack);
-int				single_reverse(t_stack *stack);
-int				double_rotate(t_stack *stack_a, t_stack *stack_b);
-int				double_reverse(t_stack *stack_a, t_stack *stack_b);
+int		single_swap(t_stack *stack);
+int		double_swap(t_stack *stack_a, t_stack *stack_b);
+int		push_ope(t_stack *src, t_stack *dst);
+int		single_rotate(t_stack *stack);
+int		single_reverse(t_stack *stack);
+int		double_rotate(t_stack *stack_a, t_stack *stack_b);
+int		double_reverse(t_stack *stack_a, t_stack *stack_b);
 
-int				exec_operation(t_stack *stack_a, t_stack *stack_b, char *ope);
+int		exec_operation(t_stack *stack_a, t_stack *stack_b, char *ope);
 
 #endif
--- checker_prog/includes/get_next_line.h
+++ checker_prog/includes/get_next_line.h
@@ -6,21 +6,21 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 18:30:23 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/05/13 09:08:10 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/05/20 08:53:52 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_GET_NEXT_LINE_H
-# define FT_GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
 # define BUFFER_SIZE 1024
 
-typedef	unsigned int	t_size;
+typedef unsigned int	t_size;
 
-typedef struct	s_lista
+typedef struct s_lista
 {
 	int				fd;
 	int				flag_eof;
@@ -31,15 +31,14 @@ typedef struct	s_lista
 	struct s_lista	*next;
 }				t_lista;
 
-int				get_next_line(int fd, char **line);
-int				ft_read_fd(t_lista *elm, char **line);
-int				ft_fill_line(t_lista *elm, char **line);
-void			ft_clear_elm(t_lista **begin_list, t_lista *elm_to_del);
-t_lista			*ft_create_elmem(int fd);
-t_lista			*ft_push_front(t_lista **begin_list, int fd);
-char			*ft_strncpy(char *dst, char *src, size_t len);
-char			*ft_realloc_spe(char *src, size_t init_len, size_t add_len);
-char			*ft_strncat(char *dst, char *src,
-										size_t line_offset, size_t add_len);
+int		get_next_line(int fd, char **line);
+int		ft_read_fd(t_lista *elm, char **line);
+int		ft_fill_line(t_lista *elm, char **line);
+void	ft_clear_elm(t_lista **begin_list, t_lista *elm_to_del);
+t_lista	*ft_create_elmem(int fd);
+t_lista	*ft_push_front(t_lista **begin_list, int fd);
+char	*ft_strncpy(char *dst, char *src, size_t len);
+char	*ft_realloc_spe(char *src, size_t init_len, size_t add_len);
+char	*ft_strncat(char *dst, char *src, size_t line_offset, size_t add_len);
 
 #endif
